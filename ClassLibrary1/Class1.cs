20fd25347a2642511985549ba69e9802527ebed4
HanaPanana <Babin.ea@yandex.ru>
2022-04-29 12:10:09 +0300

    Lesson2

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        static bool Odd(int a)
+        {
+            return a % 2 == 0;
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Введите целое число.");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x + " - " + ((Odd(x)) ? "четное число" : "нечетное число"));
+            Console.WriteLine("Для выхода из приложение нажмите Enter");
+            Console.ReadLine();
+        }
+    }
+}
